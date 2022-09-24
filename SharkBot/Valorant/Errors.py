0bca4d48e0ae9b1c919a53dbac219b74c3aaff3e
ChaosCantrip <james.donald.02@gmail.com>
2022-09-24 03:00:24 +0100

    Added Valorant.Errors.handler()

--- SharkBot/Valorant/Errors.py
+++ SharkBot/Valorant/Errors.py
@@ -17,3 +17,20 @@ class MapNotFoundError(ValorantError):
 
 class InvalidAgentValueError(ValorantError):
     pass
+
+
+async def handler(ctx: commands.Context, error: Exception) -> bool:
+
+    if isinstance(error, AgentNotFoundError):
+        await ctx.reply(f"I'm afraid {error.args} is not a valid Agent!")
+        return True
+
+    if isinstance(error, MapNotFoundError):
+        await ctx.reply(f"I'm afraid {error.args} is not a valid Map!")
+        return True
+
+    if isinstance(error, InvalidAgentValueError):
+        await ctx.reply(f"I'm afraid {error.args} is not a valid Agent Value!")
+        return True
+
+    return False
