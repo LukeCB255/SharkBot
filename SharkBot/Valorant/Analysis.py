9f420b24833532afdc600d7029f0d295ec8f5a4e
ChaosCantrip <james.donald.02@gmail.com>
2022-09-24 03:53:14 +0100

    Added Valorant.Analysis constructor

--- SharkBot/Valorant/Analysis.py
+++ SharkBot/Valorant/Analysis.py
@@ -1,2 +1,12 @@
+from SharkBot.Valorant import Errors, Map, Agent
+
+
 class Analysis:
-    pass
+
+    def __init__(self, data: dict[str, dict[str, list[int]]]):
+        self.data = {
+            Map.get(mapName): {
+                Agent.get(agentName): agentData for agentName, agentData in mapData.items()
+            } for mapName, mapData in data.items()
+        }
+
