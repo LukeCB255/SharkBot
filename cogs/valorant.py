fcb57d0e7e7797ebbd252a308eed8510f2ee6f81
ChaosCantrip <james.donald.02@gmail.com>
2022-10-17 13:01:01 +0100

    Added $val command group

--- cogs/valorant.py
+++ cogs/valorant.py
@@ -8,6 +8,10 @@ class Valorant(commands.Cog):
 	def __init__(self, bot: commands.Bot) -> None:
 		self.bot = bot
 
+	@commands.hybrid_group()
+	async def val(self, ctx: commands.Context):
+		await ctx.send("Valorant Command")
+
 		
 async def setup(bot):
 	await bot.add_cog(Valorant(bot))
