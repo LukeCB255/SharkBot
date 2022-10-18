d1c1f8dd3ef9fcb4cc28454a54d16874d986cb0d
ChaosCantrip <james.donald.02@gmail.com>
2022-10-19 00:56:16 +0100

    Removed redundant arguments

--- SharkBot/Valorant/Views/MapsSelect.py
+++ SharkBot/Valorant/Views/MapsSelect.py
@@ -1,11 +1,14 @@
 import discord
 from SharkBot import Valorant
+
 options = []
 for map in Valorant.maps:
     option = discord.SelectOption(label=map.name)
     options.append(option)
+
+
 class MapsSelect(discord.ui.Select):
-    def __init__(self, member, options=options):
+    def __init__(self, member):
         super().__init__(options=options)
         self.member = member
 
