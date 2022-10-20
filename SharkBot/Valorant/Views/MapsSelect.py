3de74f9dd37778610e6a1d967c79a2093e05fccd
ChaosCantrip <james.donald.02@gmail.com>
2022-10-20 17:31:01 +0100

    Fixed $val match removing options

--- SharkBot/Valorant/Views/MapsSelect.py
+++ SharkBot/Valorant/Views/MapsSelect.py
@@ -1,13 +1,16 @@
 import discord
 from SharkBot import Valorant
 
-options = [discord.SelectOption(label=map.name) for map in Valorant.Map.maps]
+options_with_all = [discord.SelectOption(label=map.name) for map in Valorant.Map.maps]
+options_without_all = options_with_all[1:]
 
 
 class MapsSelect(discord.ui.Select):
-    def __init__(self, rem_all: bool = False):
-        if rem_all:
-            options.remove(options[0])
+    def __init__(self, remove_all: bool = False):
+        if remove_all:
+            options = options_without_all
+        else:
+            options = options_with_all
         super().__init__(options=options)
 
     async def callback(self, interaction: discord.Interaction) -> None:
