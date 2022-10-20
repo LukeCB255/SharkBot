32d6d6082c91a40e1d242966dd0ea5f83ca7b6de
ChaosCantrip <james.donald.02@gmail.com>
2022-10-20 06:32:31 +0100

    Added Emoji and changed label of BackButton

--- SharkBot/Valorant/Views/BackButton.py
+++ SharkBot/Valorant/Views/BackButton.py
@@ -3,8 +3,8 @@ from SharkBot import Valorant
 
 
 class BackButton(discord.ui.Button):
-    def __init__(self, state: str, label="Back"):
-        super().__init__(label=label)
+    def __init__(self, state: str, label="Go Back"):
+        super().__init__(label=label, emoji="⬅")
         self.state = state
 
     async def callback(self, interaction: discord.Interaction) -> None:
