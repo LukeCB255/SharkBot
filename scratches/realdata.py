8a3ad02e35674436e39ab8ba3c1e8d7c6c99d5ce
Hxrl <harley.norgate@gmail.com>
2022-10-21 18:33:28 +0100

    scratch that uses real analysis WIP

---
+++ scratches/realdata.py
@@ -0,0 +1,61 @@
+from SharkBot import Valorant
+
+import datetime
+import random
+import itertools
+
+analysis = Valorant.Analysis.get()
+analysis = analysis.get_map_data(Valorant.Map.get("ascent"))
+
+players = [f"Player {num}" for num in range(1, 6)]
+
+agentValues = {}
+
+for player in players:
+    agentValues[player] = {}
+    for agent in analysis:
+        agentValues[player][agent] = random.randint(1, 5)
+
+optimalValues = {}
+for agent in analysis:
+    optimalValues[agent] = [random.randint(1, 5) for num in range(1, 6)]
+
+print(agentValues)
+print(optimalValues)
+
+teams = [team for team in itertools.permutations(players, 5)]
+print(len(teams))
+print(teams)
+
+agentTeams = [team for team in itertools.permutations(agents, 5)]
+print(len(agentTeams))
+
+combos = itertools.product(teams, agentTeams)
+comboResults = {}
+i = 0
+
+highestValue = 0
+c = None
+
+start = datetime.datetime.now()
+
+for combo in combos:
+    value = 0
+    for j in range(0, 5):
+        player = combo[0][j]
+        agent = combo[1][j]
+        value += agentValues[player][agent] * optimalValues[agent][j]
+
+    if value > highestValue:
+        highestValue = value
+        c = combo
+
+    if i % 1000000 == 0:
+        print(f"{i} done")
+    i += 1
+
+end = datetime.datetime.now()
+print(f"{highestValue} {c}")
+print(f"{(end - start).total_seconds()} time")
+
+print("Done")
